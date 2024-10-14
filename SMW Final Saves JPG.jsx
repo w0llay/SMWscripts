3f8b51b131aa96e19ae2d8dcd0b233a338b73919
w0llay <greblando3@gmail.com>
2024-10-14 15:09:19 -0600

    added scripts

---
+++ SMW Final Saves JPG.jsx
@@ -0,0 +1,2 @@
+#include "/Applications/Adobe Photoshop 2024/Presets/Scripts/SMWscripts/SMW Script Core.jsx"
+exportJPG(stagingPath + "/Smartwool Final Saves Temp Folder", app.activeDocument.name)
---
+++ SMW Final Saves PSD.jsx
@@ -0,0 +1,2 @@
+#include "/Applications/Adobe Photoshop 2024/Presets/Scripts/SMWscripts/SMW Script Core.jsx"
+app.activeDocument.saveAs(new Folder(stagingPath + "/Smartwool Final Saves Temp Folder"))

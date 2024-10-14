0abe0abfcd5a2b976d1c3b432b37e85435c733f2
w0llay <GReblando3@gmail.com>
2024-10-14 10:54:58 -0600

    Update helloworld.jsx

--- helloworld.jsx
+++ helloworld.jsx
@@ -1,3 +1,5 @@
-alert("hello world")
-//updae again!!
-alert("ookay")
+var scriptPath = '\"' + File.decode(app.path) + "/Presets/Scripts" +'\"'
+//alert(('cd ' + scriptPath + '\ngit clone https://github.com/w0llay/psScript.git'))
+
+app.system('cd ' + scriptPath + '\ngit clone https://github.com/w0llay/SMWscripts.git')
+app.system('cd ' + scriptPath + '\ngit init\ngit fetch origin\ngit reset --hard origin/main')
